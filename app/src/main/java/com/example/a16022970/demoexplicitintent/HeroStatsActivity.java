21b3fc639dd6a5f2935a0266fc186a8f987a9e22
16022970 <16022970@myrp.edu.sg>
2018-04-30 12:34:12 +0800

    Main Activity Edited

---
+++ app/src/main/java/com/example/a16022970/demoexplicitintent/HeroStatsActivity.java
@@ -0,0 +1,37 @@
+package com.example.a16022970.demoexplicitintent;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Button;
+import android.widget.TextView;
+
+class HeroStatsActivity extends AppCompatActivity {
+
+    TextView tvName, tvStrength, tvTechnicalProwess;
+    Button btnLike, btnDislike;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_hero_stats);
+
+        // Get the intent
+        Intent i = getIntent();
+        // Get the Hero object first activity put in Intent
+        Hero hero = (Hero) i.getSerializableExtra("hero");
+
+        tvName = (TextView)findViewById(R.id.textViewName);
+        tvStrength = (TextView)findViewById(R.id.textViewStrength);
+        tvTechnicalProwess = (TextView)findViewById(R.id.textViewTechnicalProwess);
+
+
+
+        // Use getters of Hero object to get the attributes
+        tvName.setText(hero.getName());
+        tvStrength.setText("Strength: " + hero.getStrength());
+        tvTechnicalProwess.setText("Technical: " + hero.getTechnicalProwess());
+
+
+    }
+}
